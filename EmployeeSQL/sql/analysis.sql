c3f115edc694c657fb28d0e5c3ca055eccec5188
Rafael Fernandez <rafa7860@yahoo.com>
2021-02-22 20:17:52 -0500

    adding exercise 3

--- EmployeeSQL/sql/analysis.sql
+++ EmployeeSQL/sql/analysis.sql
@@ -10,3 +10,31 @@ FROM "Employees" E
     INNER JOIN "Salary" S
     	ON E."EmployeeID" = S."EmployeeID"
 ;
+
+--2)
+--List first name, last name, and hire date for employees who were hired in 1986.
+SELECT 
+	E."FisrtName" as "first name",
+	E."LastName" as "last name",
+	E."HiredDate" as "hired date"
+FROM "Employees" E
+WHERE E."HiredDate" LIKE '%/1986'
+;
+
+--3)
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+SELECT 
+	D."DepartmentID" as "department number",
+	D."Name" as "department name",
+	E."EmployeeID" as "manager employee number",
+	E."LastName" as "last name",
+	E."FisrtName" as "first name"
+FROM
+	"DepartmentManager" DM
+	INNER JOIN "Department" D
+		ON DM."DepartmentID" = D."DepartmentID"
+	INNER JOIN "Employees" E
+		ON E."EmployeeID" = DM."EmployeeID"
+;
+		
